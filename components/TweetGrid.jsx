92cd439bc66f6e33333a1e999f83aa9174e5f4b6
mathurahravigulan <mathurah.ravigulan@edu.uwaterloo.ca>
2021-11-28 23:05:26 -0500

    edited tweet grid

--- components/TweetGrid.jsx
+++ components/TweetGrid.jsx
@@ -1,25 +1,18 @@
 import React from "react";
-
-import { SimpleGrid, Image, Box, AspectRatio } from "@chakra-ui/react";
+import Image from "next/image";
+import testimonial1 from "../public/testimonial1.png";
+import { SimpleGrid, Box } from "@chakra-ui/react";
 
 const TweetGrid = () => {
   return (
-    <Box h="100%" d="flex" justifyContent="center">
-      <SimpleGrid columns={2} spacing={10} w="100%">
-        <Box h="150px" bgColor="red">
-          {/* <Image
-            w="100%"
-            h="100%"
-            src="https://images.unsplash.com/photo-1555421689-491a97ff2040?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-            alt="Twitter placeholder"
-            objectFit="cover"
-          /> */}
-        </Box>
+    <Box d="flex" justifyContent="center">
+      <SimpleGrid columns={1} spacing={10} w="100%">
+        <Box bgColor="red"></Box>
         <Box h="150px" bgColor="red">
           <Image
             w="100%"
             h="100%"
-            src="https://images.unsplash.com/photo-1555421689-491a97ff2040?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+            src={testimonial1}
             alt="Twitter placeholder"
             objectFit="cover"
           />
@@ -30,26 +23,3 @@ const TweetGrid = () => {
 };
 
 export default TweetGrid;
-
-/** <Image
-          w="100%"
-          h="50%"
-          
-          src="https://images.unsplash.com/photo-1555421689-491a97ff2040?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-          alt="Twitter placeholder"
-        />
-      </Box>
-
-      <Image
-        mt="15px"
-        src="https://images.unsplash.com/photo-1555421689-491a97ff2040?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt="Twitter placeholder"
-      />
-      <Image
-        src="https://images.unsplash.com/photo-1555421689-491a97ff2040?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt="Twitter placeholder"
-      />
-      <Image
-        src="https://images.unsplash.com/photo-1555421689-491a97ff2040?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt="Twitter placeholder"
-      />*/
