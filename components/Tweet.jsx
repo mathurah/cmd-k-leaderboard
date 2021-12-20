5428cb33ad37f5fbdbb904a8c429616562d43f4e
mathurahravigulan <mathurah.ravigulan@edu.uwaterloo.ca>
2021-12-20 16:54:16 -0500

    Footnote partially working

---
+++ components/Tweet.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Tweet = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <p>* or control +k on Windows</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tweet;
