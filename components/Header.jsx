5fb4287cccef6269063ca2c54a9d27264b9134d7
chamod-gamage <chamodgamage26@gmail.com>
2021-12-22 16:48:59 -0500

    remove whitespace in hero

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from "./Header.module.css";
+import React from 'react';
+import styles from './Header.module.css';
 
-import { useSupabase } from "../hooks/useSupabase.js";
-import Button from "./Button";
-import Marquee from "./Marquee";
+import { useSupabase } from '../hooks/useSupabase.js';
+import Button from './Button';
+import Marquee from './Marquee';
 const Header = ({ user, handleSignOut }) => {
   const supabase = useSupabase();
   async function signOut() {
@@ -15,14 +15,17 @@ const Header = ({ user, handleSignOut }) => {
         <div className={styles.background}>
           <div className={styles.aboveBG}>
             <Marquee></Marquee>
-            <div className={styles.header}>
-              {/* <h1>Hello</h1> */}
+            <div className={styles.headerContainer}>
+              <div className={styles.header}>
+                {/* <h1>Hello</h1> */}
 
-              <h1>
-                Vote for a command menu <br></br> in your favorite apps
-              </h1>
+                <h1>
+                  Vote for a command menu <br></br> in your favorite apps
+                </h1>
+              </div>
+
+              <Button style="cta">Vote Now</Button>
             </div>
-            <Button style="cta">Vote Now</Button>
           </div>
         </div>
       </div>
