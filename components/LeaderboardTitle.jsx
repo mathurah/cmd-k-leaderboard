e686f62d14a39275e23cca75cc236ee851803aa1
chamod-gamage <chamodgamage26@gmail.com>
2021-12-24 12:17:22 -0500

    styling setup/fixes for leaderboard

--- components/LeaderboardTitle.jsx
+++ components/LeaderboardTitle.jsx
@@ -3,10 +3,8 @@ import styles from './Leaderboard.module.css';
 const LeaderboardTitle = () => {
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.text}>
-          <h2>Leaderboard</h2>
-        </div>
+      <div className={styles.text}>
+        <h2>Leaderboard</h2>
       </div>
     </>
   );
