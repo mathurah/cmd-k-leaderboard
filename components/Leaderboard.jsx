a6285b91d60a15be0fc54a96d9f16d12807fc5d8
chamod-gamage <chamodgamage26@gmail.com>
2021-12-24 12:02:26 -0500

    leaderboard instructions

--- components/Leaderboard.jsx
+++ components/Leaderboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import LeaderboardTitle from './LeaderboardTitle';
+import styles from './Leaderboard.module.css';
 
 const Leaderboard = () => {
   return (
     <>
-      <div>
+      <div className={styles.leaderboard}>
         <LeaderboardTitle />
+        <div className={styles.leaderboardContainer}>
+          <div className={styles.leaderboardInstructions}>
+            Vote for apps where you want a command bar
+          </div>
+        </div>
       </div>
     </>
   );
