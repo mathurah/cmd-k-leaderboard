e80f9adda901c7c43a9345f732badf4c52cff4aa
chamod-gamage <chamodgamage26@gmail.com>
2021-12-31 16:45:34 -0500

    gray titles beside main titles

--- components/MainTitle.jsx
+++ components/MainTitle.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import Image from "next/image";
-import rainbow from "../public/State=Rainbow 1.svg";
-import styles from "./MainTitle.module.css";
+import React from 'react';
+import Image from 'next/image';
+import rainbow from '../public/State=Rainbow 1.svg';
+import styles from './MainTitle.module.css';
+
+const RAINBOW = 'State=Rainbow 1.svg';
 
 const MainTitle = () => {
   return (
-    <>
+    <div className={styles.mainTitleContainer}>
+      <div className={styles.mainTitleLeft} />
       <div className={styles.mainTitle}>
-        <Image alt="I want cmd-k" boxSize="40px" src={rainbow} />
+        <img alt="I WANT CMD+K" src={RAINBOW} />
       </div>
-    </>
+      <div className={styles.mainTitleRight} />
+    </div>
   );
 };
 
