df8c5e089a7f6338df4bfc2c57b20b68ddae1ec1
chamod-gamage <chamodgamage26@gmail.com>
2022-01-01 18:30:08 -0500

    initial animation setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { useState, useContext } from 'react';
+import './Header.module.css';
+import { useState, useContext, useLayoutEffect, useEffect } from 'react';
 import { Store } from '../context/state';
 import { useSupabase } from '../hooks/useSupabase.js';
 import { signOut } from '../api/supabase';
 import Button from './Button';
 import Marquee from './Marquee';
 import { ACTION_TYPES } from '../context/constants';
+// import { Tween, Timeline } from 'react-gsap';
+// import { Controller, Scene } from 'react-scrollmagic';
+import { gsap, Linear, Power4 } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.js';
+import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
 
 const CMDK1 = 'Animation_Bar_1.svg';
 const ABSTRACT_COLOR = 'AbstractColor.svg';
@@ -22,10 +28,98 @@ const Header = () => {
     await signOut(supabase);
     dispatch({ type: ACTION_TYPES.SIGN_OUT });
   };
+  const [duration, setDuration] = useState(3040);
+
+  useEffect(() => {
+    window.onresize = () => {
+      if (duration !== 3040 && window.innerWidth > 700) {
+        setDuration(3040);
+      } else if (duration === 3040 && window.innerWidth < 700) {
+        setDuration(1120);
+      }
+    };
+
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollToPlugin);
+
+    function updatePercentage() {
+      //percent.innerHTML = (tl.progress() *100 ).toFixed();
+      tl.progress();
+      console.log(tl.progress());
+    }
+    // var autoScroll = gsap.timeline({}).to(window, {
+    //   duration: 100,
+    //   scrollTo: { y: 3040, autoKill: false },
+    // });
+    var tl = gsap.timeline({
+      onUpdate: updatePercentage,
+      paused: false,
+      scrollTrigger: {
+        trigger: document.querySelector('#start'),
+        pin: false,
+        start: 'top top',
+        scrub: true,
+        end: 'bottom bottom',
+        markers: true,
+      },
+    });
+
+    tl.from(
+      document.querySelector('#CMDK1'),
+      {
+        opacity: 0.7,
+        y: -250,
+        duration: 20,
+      },
+      '<'
+    );
+
+    tl.from(
+      document.querySelector('#UI1'),
+      {
+        opacity: 0.7,
+        y: -150,
+        duration: 20,
+      },
+      '<'
+    );
+
+    tl.to(
+      document.querySelector('#CMDK1'),
+      {
+        opacity: -1000,
+        duration: 80,
+        scrollTo: { y: 3500, autoKill: false },
+      },
+      '>'
+    );
+
+    tl.to(
+      document.querySelector('#UI1'),
+      {
+        opacity: -1000,
+        duration: 80,
+      },
+      '<'
+    );
+
+    var ScrollMagic = require('scrollmagic');
+
+    var controller = new ScrollMagic.Controller();
+
+    var scene = new ScrollMagic.Scene({
+      triggerElement: document.querySelector('#trigger'),
+      triggerHook: 'onLeave',
+      duration: '100%',
+    })
+      .setTween(tl)
+      // .setTween(autoScroll)
+      .addTo(controller);
+  }, []);
 
   return (
     <>
-      <div className={styles.main}>
+      <div id="start" className={styles.main}>
         <div className={styles.background}>
           <div className={styles.aboveBG}>
             <Marquee
@@ -42,6 +136,7 @@ const Header = () => {
                       }))
               }
             />
+            <div />
             <div className={styles.headerWrapper}>
               <div className={styles.headerContainerA}>
                 <div className={styles.header}>
@@ -64,7 +159,7 @@ const Header = () => {
                   )}
                 </div>
               </div>
-              <div className={styles.headerContainerB}>
+              <div id="title2" className={styles.headerContainerB}>
                 <div className={styles.header}>
                   <div className={styles.title}>
                     <h1>We love command menus</h1>
@@ -72,6 +167,7 @@ const Header = () => {
                 </div>
               </div>
             </div>
+
             <div className={styles.headerContainerC}>
               <div className={styles.header}>
                 <div className={styles.titleLong}>
@@ -84,6 +180,14 @@ const Header = () => {
                 <div className={styles.title}>
                   <h1>let's change that, together!</h1>
                 </div>
+              </div>
+            </div>
+            <div className={styles.animations} id="trigger">
+              <div id={'CMDK1'} className={styles.animationBar1}>
+                <img src={CMDK1} alt="animation bar" />
+              </div>
+              <div id={'UI1'} className={styles.abstractUI1}>
+                <img src={ABSTRACT_COLOR} alt="abstract color" />
               </div>
             </div>
           </div>
