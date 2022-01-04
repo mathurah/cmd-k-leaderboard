a342611a654977448f3aa47a6f0ed57ec2f1bd36
Mathurah Ravigulan <mathurah.ravigulan@gmail.com>
2022-01-04 15:10:20 -0800

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -9,26 +9,23 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
           />
-
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments)}
-                gtag('js', new Date());
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                
                 ga('create', 'UA-XXXXX-Y', 'auto');
+                ga('send', 'pageview');
                 ga(function(tracker) {
-                  console.log('here')
                   var clientId = tracker.get('clientId');
-                  console.log(clientId)
                 });
-                console.log(clientId)
-
-                gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
-                `
-              }}
-          />
+          `,
+            }}
+          />          
+          
         </Head>
         <body>
           <Main />
