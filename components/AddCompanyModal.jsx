eea65e2386520bbec35b00e7423b6a42c202a22b
Chamod Gamage <chamod.gamage@beondeck.com>
2022-01-04 01:37:34 -0500

    revamp of add modal

--- components/AddCompanyModal.jsx
+++ components/AddCompanyModal.jsx
@@ -1,34 +1,40 @@
-import { Text, Checkbox, Box, Input, Button, Image } from "@chakra-ui/react";
-import Autosuggest from "react-autosuggest";
-import themeable from "react-themeable";
-import { useState, useContext } from "react";
-import Modal from "./Modal";
-import { Store } from "../context/state";
-import { insertOption } from "../api/supabase";
-import { ACTION_TYPES } from "../context/constants";
-import styles from "./Modal.module.css";
+import { Text, Checkbox, Box, Input, Image } from '@chakra-ui/react';
+import Autosuggest from 'react-autosuggest';
+import themeable from 'react-themeable';
+import { useState, useContext } from 'react';
+import Modal from './Modal';
+import { Store } from '../context/state';
+import { insertOption } from '../api/supabase';
+import { ACTION_TYPES } from '../context/constants';
+import styles from './Modal.module.css';
+import Button from './Button';
 
-const AddCompanyModal = ({ Toggle }) => {
-  const [name, setName] = useState("");
-  const [query, setQuery] = useState("");
-  const [url, setUrl] = useState("");
+const AddCompanyModal = ({ Toggle, toggleVote }) => {
+  const [name, setName] = useState('');
+  const [query, setQuery] = useState('');
+  const [url, setUrl] = useState('');
   const [selected, setSelected] = useState(null);
   const [submitted, setSubmitted] = useState(null);
   const [isUser, setIsUser] = useState(false);
   const [companyOptions, setCompanyOptions] = useState([]);
 
   const {
-    state: { showAdd: show, voteOptions, user },
+    state: { showAdd: show, voteOptions, user, userVotes },
     dispatch,
   } = useContext(Store);
 
   const currentOptions = voteOptions.map((option) => option.url);
+  const currentVotes = voteOptions.reduce(
+    (map, option) => ((map[option.url] = option), map),
+    {}
+  );
 
   const clearOptions = () => {
     setCompanyOptions([]);
-    setName("");
-    setUrl("");
-    setQuery("");
+    setName('');
+    setUrl('');
+    setQuery('');
+    setSelected(null);
     setIsUser(false);
   };
 
@@ -70,20 +76,38 @@ const AddCompanyModal = ({ Toggle }) => {
   const getCompanyURL = (company) => company.domain;
   const getCompanyName = (company) => company.name;
 
-  const renderCompanyOption = (company) => (
-    <Box display="flex" flexDirection="row" p="1rem">
-      <Image height="5rem" alt="Company logo" src={`${company.logo}`}></Image>
-      <Box p="1rem">
-        <Text as="b">{company.name} </Text>
-        <Text>{company.domain}</Text>
+  const InnerCompanyOption = ({ company }) => (
+    <>
+      <div className={styles.companyAutoOptionLeft}>
+        <div className={styles.companyAutoOptionLogo}>
+          <img src={`${company.logo}`} alt={company.name} />
+        </div>
+        <div className={styles.companyAutoOptionText}>
+          <div className={styles.companyAutoOptionName}>{company.name}</div>
+          <div className={styles.companyAutoOptionDomain}>{company.domain}</div>
+        </div>
+      </div>
+      {currentVotes.hasOwnProperty(company.domain) && (
+        <div className={styles.companyAutoOptionRight}>
+          <div className={styles.companyAutoOptionVoted}>
+            <div className={styles.companyAutoOptionPlus}>+</div>
+            <div className={styles.companyAutoOptionVotes}>
+              {currentVotes[company.domain].votes}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 
-        {/* Need to add logic if company is already selected  */}
-      </Box>
-    </Box>
+  const renderCompanyOption = (company) => (
+    <div className={styles.companyAutoOption}>
+      <InnerCompanyOption company={company} />
+    </div>
   );
 
   const autosuggestInputProps = {
-    placeholder: "Search for a company",
+    placeholder: 'Search for a company',
     value: query,
     name,
     onChange: (event, { newValue }) => {
@@ -122,7 +146,42 @@ const AddCompanyModal = ({ Toggle }) => {
         </div>
       </div>
 
-      <Box d="flex" alignItems="center" justifyContent="center">
+      {/* <Box d="flex" alignItems="center" justifyContent="center"> */}
+
+      {/* </Box> */}
+      {selected !== null ? (
+        <div className={styles.companySelector}>
+          <div className={styles.companyOption}>
+            <InnerCompanyOption company={selected} />
+          </div>
+          <div
+            onClick={() => {
+              setSelected(null);
+              setSubmitted(false);
+            }}
+            className={styles.selectCancel}
+          >
+            <svg
+              width="36"
+              height="36"
+              viewBox="0 0 36 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15 18.0284L6 8.97872L8.9717 6L17.9434 15.0213L27.0566 6L30 8.92199L20.9151 18.0284L29.9434 27.1064L27.0566 30L17.9434 21.0071L8.9717 29.9433L6.0566 27.0213L15 18.0284Z"
+                fill="white"
+              />
+              <path
+                d="M4.58412 7.56618L3.17712 8.97651L4.5819 10.389L12.1793 18.0284L4.63849 25.611L3.23373 27.0235L4.64072 28.4338L7.55582 31.3558L8.96722 32.7705L10.3831 31.3603L17.95 23.8234L25.6518 31.4236L27.0676 32.8207L28.4725 31.4125L31.3593 28.5189L32.7663 27.1086L31.3615 25.6961L23.738 18.0306L31.4159 10.3345L32.8319 8.91515L31.409 7.50262L28.4656 4.58063L27.0586 3.18382L25.6496 4.57864L17.9544 12.1961L10.3898 4.58969L8.97393 3.16599L7.55582 4.58746L4.58412 7.56618Z"
+                stroke="black"
+                stroke-opacity="0.16"
+                stroke-width="4"
+              />
+            </svg>
+          </div>
+        </div>
+      ) : (
         <Autosuggest
           suggestions={companyOptions || []}
           onSuggestionsFetchRequested={onSuggestionsFetchRequested}
@@ -132,35 +191,23 @@ const AddCompanyModal = ({ Toggle }) => {
           renderSuggestion={renderCompanyOption}
           inputProps={autosuggestInputProps}
         />
-      </Box>
-      {selected !== null && (
-        <div className={styles.companySelector}>
-          <div className={styles.companyOption}>
-            <Image
-              className={styles.companyImage}
-              height="4rem"
-              alt="Company logo"
-              src={`${selected.logo}`}
-            ></Image>
-            <Box>
-              <Text as="b">{selected.name} </Text>
-              <Text>{selected.domain}</Text>
-            </Box>
-          </div>
-          <Box
-            w="100%"
-            m="2rem"
-            d="flex"
-            justifyContent="center"
-            pt="15px"
-            pb="15px"
-          ></Box>
-        </div>
       )}
-      {!submitted ? (
-        currentOptions.includes(url) ? (
-          <Button m="2.1rem" size="sm" colorScheme="purple" disabled>
-            Company Already Added
+      <div className={styles.addBtnContainer}>
+        {selected && currentVotes.hasOwnProperty(selected.domain) ? (
+          <Button
+            onClick={() => {
+              toggleVote(currentVotes[selected.domain].id);
+              Toggle();
+              setSubmitted(true);
+              clearOptions();
+            }}
+            style="addModal"
+          >
+            {userVotes
+              .map(({ option_id }) => option_id)
+              .includes(currentVotes[selected.domain].id)
+              ? 'Remove Vote'
+              : 'Vote'}
           </Button>
         ) : (
           <Button
@@ -170,18 +217,12 @@ const AddCompanyModal = ({ Toggle }) => {
               clearOptions();
               Toggle();
             }}
-            m="2.1rem"
-            size="sm"
-            colorScheme="purple"
+            style="addModal"
           >
             Add company
           </Button>
-        )
-      ) : (
-        <Button m="2.1rem" size="sm" colorScheme="purple" disabled>
-          Company Added
-        </Button>
-      )}
+        )}
+      </div>
     </Modal>
   );
 };
