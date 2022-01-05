89dfb9b23199a594caccc3aebf47f02ebf6e5989
Wes Feller <wes@commandbar.com>
2022-01-04 19:35:41 -0800

    feat(auth): add google analytics
    Next.js has a specific method for adding Google Analytics.
    
    Script component must be used: https://nextjs.org/docs/messages/next-script-for-ga
    Script component cannot be used in pages/_document: https://nextjs.org/docs/messages/no-script-in-document-page

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,43 @@
-import '@fontsource/inter';
-import './globals.css';
-import { StoreProvider } from '../context/state';
-import { initialState } from '../context/constants';
-import { reducer } from '../context/reducer';
+import Script from "next/script";
+import "@fontsource/inter";
+import "./globals.css";
+import { StoreProvider } from "../context/state";
+import { initialState } from "../context/constants";
+import { reducer } from "../context/reducer";
 import {
   ChakraProvider,
   Container,
   Stack,
   Heading,
   Text,
-} from '@chakra-ui/react';
-
+} from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider >
-      <StoreProvider initialState={initialState} reducer={reducer}>
-        <Component {...pageProps} />
-      </StoreProvider>
-    </ChakraProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          gtag('get', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', 'client_id', (client_id) => {
+            localStorage.setItem('client_id', client_id)
+          })
+        `}
+      </Script>
+
+      <ChakraProvider>
+        <StoreProvider initialState={initialState} reducer={reducer}>
+          <Component {...pageProps} />
+        </StoreProvider>
+      </ChakraProvider>
+    </>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -9,23 +9,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
             rel="stylesheet"
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-                
-                ga('create', 'UA-XXXXX-Y', 'auto');
-                ga('send', 'pageview');
-                ga(function(tracker) {
-                  var clientId = tracker.get('clientId');
-                });
-          `,
-            }}
-          />          
-          
         </Head>
         <body>
           <Main />
