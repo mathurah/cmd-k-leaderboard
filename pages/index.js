ceacf25f08d264aa289de89fee0d01e20a83d014
Mathurah Ravigulan <mathurah25@gmail.com>
2022-01-20 09:37:27 -0800

    test image linik

--- pages/index.js
+++ pages/index.js
@@ -174,7 +174,10 @@ export default function Home() {
             name="twitter:description"
             content="Vote for a commandbar on your favorite websites"
           />
-          <meta name="twitter:image" content="../twitter-image.svg" />
+          <meta
+            name="twitter:image"
+            content="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*"
+          />
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header />
