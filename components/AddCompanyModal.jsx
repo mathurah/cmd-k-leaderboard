6e4a0e21bfac37f0d24f32392ea2a67ee8becad3
Chamod Gamage <chamod.gamage@beondeck.com>
2022-01-20 23:03:13 -0500

    recenter on desktop add company modal

--- components/AddCompanyModal.jsx
+++ components/AddCompanyModal.jsx
@@ -186,50 +186,52 @@ const AddCompanyModal = ({ toggle, toggleVote }) => {
         {/* <Box d="flex" alignItems="center" justifyContent="center"> */}
 
         {/* </Box> */}
-        {selected !== null ? (
-          <div className={styles.companySelector}>
-            <div className={styles.companyOption}>
-              <InnerCompanyOption company={selected} />
-            </div>
-            <div
-              onClick={() => {
-                setSelected(null);
-                setSubmitted(false);
-              }}
-              className={styles.selectCancel}
-            >
-              <svg
-                width="36"
-                height="36"
-                viewBox="0 0 36 36"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+        <div className={styles.addCompanyModalBody}>
+          {selected !== null ? (
+            <div className={styles.companySelector}>
+              <div className={styles.companyOption}>
+                <InnerCompanyOption company={selected} />
+              </div>
+              <div
+                onClick={() => {
+                  setSelected(null);
+                  setSubmitted(false);
+                }}
+                className={styles.selectCancel}
               >
-                <path
-                  d="M15 18.0284L6 8.97872L8.9717 6L17.9434 15.0213L27.0566 6L30 8.92199L20.9151 18.0284L29.9434 27.1064L27.0566 30L17.9434 21.0071L8.9717 29.9433L6.0566 27.0213L15 18.0284Z"
-                  fill="white"
-                />
-                <path
-                  d="M4.58412 7.56618L3.17712 8.97651L4.5819 10.389L12.1793 18.0284L4.63849 25.611L3.23373 27.0235L4.64072 28.4338L7.55582 31.3558L8.96722 32.7705L10.3831 31.3603L17.95 23.8234L25.6518 31.4236L27.0676 32.8207L28.4725 31.4125L31.3593 28.5189L32.7663 27.1086L31.3615 25.6961L23.738 18.0306L31.4159 10.3345L32.8319 8.91515L31.409 7.50262L28.4656 4.58063L27.0586 3.18382L25.6496 4.57864L17.9544 12.1961L10.3898 4.58969L8.97393 3.16599L7.55582 4.58746L4.58412 7.56618Z"
-                  stroke="black"
-                  strokeOpacity="0.16"
-                  strokeWidth="4"
-                />
-              </svg>
+                <svg
+                  width="36"
+                  height="36"
+                  viewBox="0 0 36 36"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15 18.0284L6 8.97872L8.9717 6L17.9434 15.0213L27.0566 6L30 8.92199L20.9151 18.0284L29.9434 27.1064L27.0566 30L17.9434 21.0071L8.9717 29.9433L6.0566 27.0213L15 18.0284Z"
+                    fill="white"
+                  />
+                  <path
+                    d="M4.58412 7.56618L3.17712 8.97651L4.5819 10.389L12.1793 18.0284L4.63849 25.611L3.23373 27.0235L4.64072 28.4338L7.55582 31.3558L8.96722 32.7705L10.3831 31.3603L17.95 23.8234L25.6518 31.4236L27.0676 32.8207L28.4725 31.4125L31.3593 28.5189L32.7663 27.1086L31.3615 25.6961L23.738 18.0306L31.4159 10.3345L32.8319 8.91515L31.409 7.50262L28.4656 4.58063L27.0586 3.18382L25.6496 4.57864L17.9544 12.1961L10.3898 4.58969L8.97393 3.16599L7.55582 4.58746L4.58412 7.56618Z"
+                    stroke="black"
+                    strokeOpacity="0.16"
+                    strokeWidth="4"
+                  />
+                </svg>
+              </div>
             </div>
-          </div>
-        ) : (
-          <Autosuggest
-            suggestions={companyOptions || []}
-            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-            onSuggestionsClearRequested={onSuggestionsClearRequested}
-            getSuggestionValue={getCompanyName}
-            onSuggestionSelected={onSuggestionSelected}
-            renderSuggestion={renderCompanyOption}
-            inputProps={autosuggestInputProps}
-          />
-        )}
-        <AddBtn className={styles.addBtnContainer} />
+          ) : (
+            <Autosuggest
+              suggestions={companyOptions || []}
+              onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+              onSuggestionsClearRequested={onSuggestionsClearRequested}
+              getSuggestionValue={getCompanyName}
+              onSuggestionSelected={onSuggestionSelected}
+              renderSuggestion={renderCompanyOption}
+              inputProps={autosuggestInputProps}
+            />
+          )}
+          <AddBtn className={styles.addBtnContainer} />
+        </div>
       </div>
     </Modal>
   );
